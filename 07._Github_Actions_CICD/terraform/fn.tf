0cb6368d381fc6fd922c75384d54b4d736ffd05d
anderslatif <anderslatif@users.noreply.github.com>
2024-03-20 16:58:51 +0100

    Added the Github Actions tutorial

---
+++ 07._Github_Actions_CICD/terraform/fn.tf
@@ -0,0 +1,53 @@
+
+resource "azurerm_storage_account" "functions" {
+  name                     = "st${var.application_name}${var.environment_name}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_user_assigned_identity" "functions" {
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  name                = "mi-${var.application_name}-${var.environment_name}-fn"
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "asp-${var.application_name}-${var.environment_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "foo" {
+  name                = "func-${var.application_name}-${var.environment_name}-foo"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  storage_account_name       = azurerm_storage_account.functions.name
+  storage_account_access_key = azurerm_storage_account.functions.primary_access_key
+  service_plan_id            = azurerm_service_plan.main.id
+
+  site_config {
+    application_insights_key               = azurerm_application_insights.main.instrumentation_key
+    application_insights_connection_string = azurerm_application_insights.main.connection_string
+
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    cors {
+      allowed_origins     = ["https://portal.azure.com"]
+      support_credentials = true
+    }
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = 1
+  }
+
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.functions.id]
+  }
+}
